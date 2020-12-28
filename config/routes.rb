75cbdd4bdb4425455a20a95065107bfb7b052bbe
Albert Català <albert.catala@rakuten.com>
2020-12-28 11:42:23 +0100

    Create Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'seasons' => 'seasons#index'
+      get 'movies' => 'movies#index'
+      get 'contents' => 'contents#index'
+      get 'library' => 'purchases#index'
+
+      resources :purchase_options, only: :index do
+        resources :purchases, only: :create
+      end
+    end
+  end
 end
