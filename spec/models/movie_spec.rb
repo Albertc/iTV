0a28eea88a103e42fc452ca3f540063094f9dc6e
Albert Català <albert.catala@rakuten.com>
2020-12-28 12:16:00 +0100

    Create test for Movie model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:purchase_options).dependent(:destroy) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:plot) }
+
+  describe 'ordered scope' do
+    let!(:second_movie) { create(:movie) }
+    let!(:first_movie) { create(:movie, created_at: Date.yesterday) }
+
+    subject { Movie.all.ordered }
+
+    it do
+      is_expected.to eq([first_movie, second_movie])
+    end
+  end
 end
