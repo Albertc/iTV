d8ea483ecfe2abe450f8ddaf175abb2c383fbde1
Albert Català <albert.catala@rakuten.com>
2020-12-28 11:59:03 +0100

    Create concern authenticable
    Just with one method to find de user received in params['user_id']

---
+++ app/controllers/concerns/api/v1/authenticable.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Authenticable
+      extend ActiveSupport::Concern
+
+      included do
+        before_action :authenticate
+      end
+
+      private
+
+      def authenticate
+        # As README indicates "There is no need to implement authentication
+        # nor authorization**. Suppose that the user is already registered
+        # and authenticated, you can identify the user in each request
+        # by a parameter like user_id. we assume there is a authenticated user"
+        @current_user = User.find(params[:user_id])
+      resque ActiveRecord::RecordNotFound
+        @current_user = nil
+      end
+    end
+  end
+end
